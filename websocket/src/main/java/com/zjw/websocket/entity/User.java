a0bd5895f33100dfb0b015518c50ccdded3c6652
e <1945192314@qq.com>
2021-01-16 15:21:09 +0800

    update

---
+++ websocket/src/main/java/com/zjw/websocket/entity/User.java
@@ -0,0 +1,15 @@
+package com.zjw.websocket.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private Long id;
+    private String username;
+    private String password;
+}
--- websocket/src/test/java/com/zjw/websocket/WebsocketApplicationTests.java
+++ websocket/src/test/java/com/zjw/websocket/WebsocketApplicationTests.java
@@ -1,13 +1,31 @@
 package com.zjw.websocket;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @SpringBootTest
 class WebsocketApplicationTests {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Test
-    void contextLoads() {
+    public void jdbcTest() {
+        String sql = "select * from user";
+        List<Map<String, Object>> list =  jdbcTemplate.queryForList(sql);
+        for (Map<String, Object> map : list) {
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                System.out.print(entry.getKey() + " : " + entry.getValue() + "\t");
+            }
+            System.out.println();
+        }
     }
 
 }
