699bb492c983acea1b709e82fc03197081ff318b
zhangjiwei1221 <1945192314@qq.com>
2021-01-19 19:53:44 +0800

    update stomp

--- stomp/src/main/java/com/zjw/stomp/controller/BroadcastController.java
+++ stomp/src/main/java/com/zjw/stomp/controller/BroadcastController.java
@@ -1,7 +1,5 @@
 package com.zjw.stomp.controller;
 
-import com.zjw.stomp.entity.ClientMessage;
-import com.zjw.stomp.entity.ServerMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +9,8 @@ public class BroadcastController {
 
     @MessageMapping("/sendMsg")
     @SendTo("/broadcast/greet")
-    public ServerMessage broadcast(ClientMessage clientMessage) {
-        String msg = clientMessage.getMsg();
-        return new ServerMessage(msg);
+    public String broadcast(String msg) {
+        return msg;
     }
 
 }
