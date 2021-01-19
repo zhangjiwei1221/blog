57cb20ccc4768aae975179b1aae5ef8c1680acd5
zhangjiwei1221 <1945192314@qq.com>
2021-01-19 17:17:47 +0800

    init stomp

---
+++ stomp/src/main/java/com/zjw/stomp/StompApplication.java
@@ -0,0 +1,13 @@
+package com.zjw.stomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StompApplication.class, args);
+    }
+
+}
---
+++ stomp/src/test/java/com/zjw/stomp/StompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjw.stomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StompApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
