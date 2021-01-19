f351dd13189906005643c872c7d231826b6d13c7
zhangjiwei1221 <1945192314@qq.com>
2021-01-19 16:57:00 +0800

    update

---
+++ websocket/src/main/java/com/zjw/websocket/config/TomcatConfiguration.java
@@ -0,0 +1,36 @@
+package com.zjw.websocket.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.websocket.server.WsSci;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfiguration {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        return tomcat;
+    }
+
+    private Connector createSslConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+
+    @Bean
+    public TomcatContextCustomizer tomcatContextCustomizer() {
+        return context -> context.addServletContainerInitializer(new WsSci(), null);
+    }
+
+
+}
