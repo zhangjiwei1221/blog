8414d2a3e2893b6d934cb1c6cd09bf4b870bbfff
zhangjiwei1221 <1945192314@qq.com>
2021-01-20 11:16:51 +0800

    update stomp

--- stomp/src/main/java/com/zjw/stomp/config/WebSocketConfig.java
+++ stomp/src/main/java/com/zjw/stomp/config/WebSocketConfig.java
@@ -17,7 +17,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").withSockJS();
+        registry.addEndpoint("/websocket");
     }
 
 }
