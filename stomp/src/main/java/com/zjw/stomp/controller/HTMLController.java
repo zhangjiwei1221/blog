d6d3bfc237510036b7c4e12ed249bfa92a0eb9e6
zhangjiwei1221 <1945192314@qq.com>
2021-01-20 11:25:48 +0800

    update stomp

--- stomp/src/main/java/com/zjw/stomp/controller/HTMLController.java
+++ stomp/src/main/java/com/zjw/stomp/controller/HTMLController.java
@@ -3,12 +3,6 @@ package com.zjw.stomp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * HTMLController
- *
- * @author zjw
- * @createTime 2021/1/19 18:03
- */
 @Controller
 public class HTMLController {
 
