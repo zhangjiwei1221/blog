24f53cae287b09425ae18dd48a14208b0f645a82
zhangjiwei1221 <1945192314@qq.com>
2021-01-22 16:05:57 +0800

    update mongo

--- mongo/src/main/java/cn/zjw/mongo/Main.java
+++ mongo/src/main/java/cn/zjw/mongo/Main.java
@@ -1,14 +1,9 @@
 package cn.zjw.mongo;
 
-import cn.zjw.mongo.entity.Address;
-import cn.zjw.mongo.entity.User;
-import cn.zjw.mongo.util.MongoJdbc;
+import cn.zjw.mongo.entity.TestEntity;
 import com.google.gson.Gson;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 /**
  * Main
@@ -20,22 +15,8 @@ public class Main {
 
     public static void main(String[] args) {
         Gson gson = new Gson();
-        MongoDatabase database = MongoJdbc.getDatabase();
-        MongoCollection<Document> userCollection = database.getCollection("user");
-        MongoCollection<Document> addressCollection = database.getCollection("address");
-        String[] addresses = { "河南", "江苏", "浙江", "上海" };
-        String[] names = { "张三", "李四", "王二", "赵六" };
-        int i = 0;
-        for (String addressName : addresses) {
-            FindIterable<Document> address = addressCollection.find(Filters.eq("addressName", addressName));
-            for (Document document : address) {
-                Address tmp = gson.fromJson(gson.toJson(document), Address.class);
-                User user = new User();
-                user.setUsername(names[i++]);
-                user.setAddress(tmp.get_id());
-                userCollection.insertOne(gson.fromJson(gson.toJson(user), Document.class));
-            }
-        }
+        TestEntity id = new TestEntity(new ObjectId("600a47a0076abd67f0d588f6"));
+        System.out.println(gson.fromJson(gson.toJson(id), Document.class));
     }
 
 }
