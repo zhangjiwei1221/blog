660412408eb33b5d5d4559d869bd34cc13e10994
zhangjiwei1221 <1945192314@qq.com>
2021-01-23 16:31:27 +0800

    add transactional

---
+++ transactional/src/main/java/cn/zjw/transactional/TransactionalApplication.java
@@ -0,0 +1,13 @@
+package cn.zjw.transactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionalApplication.class, args);
+    }
+
+}
---
+++ transactional/src/test/java/cn/zjw/transactional/TransactionalApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zjw.transactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
