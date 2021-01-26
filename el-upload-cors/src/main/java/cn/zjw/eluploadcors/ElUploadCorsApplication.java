513cfe4dbe3fcab1f26d290c24f90db37304ea34
zhangjiwei1221 <1945192314@qq.com>
2021-01-26 20:17:28 +0800

    add el

---
+++ el-upload-cors/src/main/java/cn/zjw/eluploadcors/ElUploadCorsApplication.java
@@ -0,0 +1,16 @@
+package cn.zjw.eluploadcors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zjw
+ */
+@SpringBootApplication
+public class ElUploadCorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElUploadCorsApplication.class, args);
+    }
+
+}
---
+++ el-upload-cors/src/test/java/cn/zjw/eluploadcors/ElUploadCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zjw.eluploadcors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElUploadCorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
