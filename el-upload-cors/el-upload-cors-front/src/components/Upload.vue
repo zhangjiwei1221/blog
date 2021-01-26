866cadb9962ec61c89f12e487788056e2f8cd607
zhangjiwei1221 <1945192314@qq.com>
2021-01-26 21:34:29 +0800

    update el

--- el-upload-cors/el-upload-cors-front/src/components/Upload.vue
+++ el-upload-cors/el-upload-cors-front/src/components/Upload.vue
@@ -8,7 +8,6 @@
       :file-list="fileList"
       list-type="picture">
       <el-button size="small" type="primary">点击上传</el-button>
-      <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
     </el-upload>
   </div>
 </template>
--- el-upload-cors/el-upload-cors-front/src/main.js
+++ el-upload-cors/el-upload-cors-front/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index'
+import 'element-ui/lib/theme-chalk/index.css'
 
 
 Vue.use(ElementUI)
