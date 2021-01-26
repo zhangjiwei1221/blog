4d2ac4836da40f120ded78336bf3c8b5ab1ba7ee
zhangjiwei1221 <1945192314@qq.com>
2021-01-26 20:59:56 +0800

    update ul

---
+++ el-upload-cors/el-upload-cors-back/src/main/java/cn/zjw/eluploadcorsback/ElUploadCorsBackApplication.java
@@ -0,0 +1,16 @@
+package cn.zjw.eluploadcorsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zjw
+ */
+@SpringBootApplication
+public class ElUploadCorsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElUploadCorsBackApplication.class, args);
+    }
+
+}
---
+++ el-upload-cors/el-upload-cors-back/src/test/java/cn/zjw/eluploadcorsback/ElUploadCorsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zjw.eluploadcorsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElUploadCorsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
