4900b17d6f9a505683473a7006e7dbf58cf22c50
zhangjiwei1221 <1945192314@qq.com>
2021-01-30 11:08:32 +0800

    add jwt

---
+++ jwt/jwt-back/src/main/java/cn/zjw/jwtback/JwtBackApplication.java
@@ -0,0 +1,16 @@
+package cn.zjw.jwtback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zjw
+ */
+@SpringBootApplication
+public class JwtBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtBackApplication.class, args);
+    }
+
+}
