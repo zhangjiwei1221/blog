ee384929faf5f426b9c23bf63ce80b78ec53bd72
zhangjiwei1221 <1945192314@qq.com>
2021-02-01 15:20:21 +0800

    add chat

---
+++ chat/chat-back/src/main/java/cn/zjw/chatback/ChatBackApplication.java
@@ -0,0 +1,13 @@
+package cn.zjw.chatback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatBackApplication.class, args);
+    }
+
+}
---
+++ chat/chat-back/src/test/java/cn/zjw/chatback/ChatBackApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zjw.chatback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
