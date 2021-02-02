1ac6875d543363c5033e4249f310e6f9bd338a0b
zhangjiwei1221 <1945192314@qq.com>
2021-02-02 21:06:08 +0800

    add sroll

--- scroll/js/index.js
+++ scroll/js/index.js
@@ -1,4 +1,9 @@
+// 默认将滚动条置为最下端
 const mid = document.querySelector('.middle')
 mid.scrollTop = mid.scrollHeight
 
+// 设置滚动条的颜色
 const setColor = color => document.documentElement.style.setProperty('--scroll-color', `${color}`)
+
+// 添加模拟信息数据
+document.getElementsByClassName('middle')[0].innerHTML = ' <div class="msg">信息...</div>'.repeat(10)
