6e622eac87ffc1285882dc4e103d1ebcb5516e8d
zhangjiwei1221 <1945192314@qq.com>
2021-02-02 22:06:11 +0800

    update scroll

--- scroll/js/index.js
+++ scroll/js/index.js
@@ -1,9 +1,37 @@
-// 默认将滚动条置为最下端
-const mid = document.querySelector('.middle')
-mid.scrollTop = mid.scrollHeight
+window.onload = () => {
+    // 添加模拟信息数据
+    addData('<div class="msg">信息...</div>'.repeat(10))
+    // 默认将滚动条置为最下端
+    setScrollToEnd()
+}
 
 // 设置滚动条的颜色
-const setColor = color => document.documentElement.style.setProperty('--scroll-color', `${color}`)
+function setColor(color) {
+    document.documentElement.style.setProperty('--scroll-color', `${color}`)
+}
 
-// 添加模拟信息数据
-document.getElementsByClassName('middle')[0].innerHTML = ' <div class="msg">信息...</div>'.repeat(10)
+function sendMsg() {
+    const node = document.querySelector('.messageText')
+    if (!node.value) {
+        node.placeholder = '信息不可为空！'
+        return
+    }
+    addData(`<div class="msg" style="text-align: right;">${node.value}</div>`)
+    setScrollToEnd()
+    node.value = node.placeholder = ''
+}
+
+function addData(msg) {
+    document.getElementsByClassName('middle')[0].innerHTML += msg
+}
+
+function setScrollToEnd() {
+    const mid = document.querySelector('.middle')
+    mid.scrollTop = mid.scrollHeight
+}
+
+function send(code) {
+    if (code === 'Enter'){
+       sendMsg()
+    }
+}
