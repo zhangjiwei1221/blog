99782861b39bed953f066190778be9be4fd513bb
zhangjiwei <1945192314@qq.com>
2021-12-19 21:29:02 +0800

    feat: 新增查看编辑框及删除提示框

--- tree/tree-front/src/views/SysDept.vue
+++ tree/tree-front/src/views/SysDept.vue
@@ -26,14 +26,46 @@
             align="center"
             label="操作">
           <template slot-scope="scope">
-            <el-button type="text" size="small">新增</el-button>
+            <el-button type="text" size="small" @click="infoOrEdit = true">新增</el-button>
             <el-button type="text" size="small">查看</el-button>
             <el-button type="text" size="small">编辑</el-button>
-            <el-button type="text" size="small" @click="remove(scope.row)">删除</el-button>
+            <el-popconfirm
+                confirm-button-text='好的'
+                cancel-button-text='不用了'
+                icon="el-icon-info"
+                icon-color="red"
+                title="确认删除该条数据吗？"
+                style="margin-left: 10px;"
+                @confirm="remove(scope.row)">
+              <el-button
+                  type="text"
+                  size="small"
+                  slot="reference">
+                删除
+              </el-button>
+            </el-popconfirm>
           </template>
         </el-table-column>
       </el-table>
     </el-main>
+    <el-dialog :visible.sync="infoOrEdit">
+      <el-form :model="deptForm">
+        <el-form-item label="名称" label-width="120px">
+          <el-input
+              v-model="deptForm.name"
+              autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="编码" label-width="120px">
+          <el-input
+              v-model="deptForm.code"
+              autocomplete="off"/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="infoOrEdit = false">取 消</el-button>
+        <el-button type="primary" @click="infoOrEdit = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-container>
 </template>
 
@@ -45,7 +77,12 @@ export default {
   data() {
     return {
       deptTree: [],
-      deptTreeSelect: []
+      deptTreeSelect: [],
+      infoOrEdit: false,
+      deptForm: {
+        name: '',
+        code: ''
+      }
     }
   },
   created() {
